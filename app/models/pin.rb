ee91fe5af416278662ee40d4d3cd3b45fad855fc
Shawn Duan <duan1216@gmail.com>
2014-04-09 23:06:49 -0400

    Revised MissingRequiredValidatorError

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,8 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
-	
-	do_not_validate_attachment_file_type :image
+
+	# do_not_validate_attachment_file_type :image
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	# validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+	do_not_validate_attachment_file_type :image
 end
