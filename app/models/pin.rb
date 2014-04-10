b1324e7c55c4a328cb8863995080c107617ab945
Shawn Duan <duan1216@gmail.com>
2014-04-10 19:54:29 -0400

    Add name to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,5 +4,10 @@ class Pin < ActiveRecord::Base
 	# do_not_validate_attachment_file_type :image
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	# validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+	
 	do_not_validate_attachment_file_type :image
+	validates :description, presence: true
+	validates :image, presence: true
+
+
 end
