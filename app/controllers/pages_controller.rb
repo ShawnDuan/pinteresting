fce494624637d0a6c38165a685be297c76083b57
Shawn Duan <duan1216@gmail.com>
2014-09-24 22:32:13 -0700

    add 2048 game

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  # def p2048
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Pinteresting::Application.routes.draw do
   devise_for :users
   root "pins#index"
   get "about" => "pages#about"  # creat about_path
+  get "2048" => "pages#2048"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
