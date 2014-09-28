501e776ae6f72b749651e9051d16aca0b29f8592
Shawn Duan <duan1216@gmail.com>
2014-09-28 14:41:05 -0700

    Modified bootstrap and customization file

--- app/controllers/resource_controller.rb
+++ app/controllers/resource_controller.rb
@@ -1,6 +1,6 @@
 class ResourceController < ApplicationController
   def files
-  	pdf_filename = File.join(Rails.root, "files/NEW_RESUME_0920.pdf")
+  	pdf_filename = File.join(Rails.root, "app/assets/files/NEW_RESUME.pdf")
   	send_file pdf_filename, :type => "application/pdf", :disposition => 'inline' 
   end
 end
